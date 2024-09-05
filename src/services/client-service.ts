794864ce07e1cb478a593e53f7fc44c0ea96606e
Naingwin <naingwin.dev@email.com>
2024-09-05 15:51:49 +0630

    key name

--- src/services/client-service.ts
+++ src/services/client-service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //apikey
-const apikey = import.meta.env.VITE_API_KEY;
+const apikey = import.meta.env.VITE_RAWG_API_KEY;
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
